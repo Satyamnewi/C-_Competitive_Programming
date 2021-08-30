14609770e21968374a5d7869bcdaa3151eb60d29
Satyamnewi <satyampathaknewid@gmail.com>
2021-08-30 15:05:58 +0530

    Hackerrank Maps

--- 34.cpp
+++ 34.cpp
@@ -1,2 +1,32 @@
 // HackerEarth - Maps-STL - https://www.hackerrank.com/challenges/cpp-maps/problem
+#include<bits/stdc++.h>
+using namespace std;
 
+const int N = 1e5;
+int a[N];
+int main()
+{
+    map<string,int> m;
+    int t;
+    cin >> t;
+
+    while(t--)
+    {
+        int x;
+        cin >> x;
+        string s;
+        cin >> s;
+        int marks;
+
+        if(x==1)
+        {
+            cin >> marks;
+            m[s] = m[s] + marks;
+        }
+        else if(x==2)
+            m.erase(s);
+        else
+            cout << m[s] << endl;
+    }
+    return 0; 
+}
--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,36 +1,32 @@
-// Vectors Basic Application
+// HackerEarth - Maps-STL - https://www.hackerrank.com/challenges/cpp-maps/problem
 #include<bits/stdc++.h>
 using namespace std;
 
 const int N = 1e5;
-char a[N];
+int a[N];
 int main()
 {
-    vector<int> v;
-    int n,x;
-    cin >> n;
+    map<string,int> m;
+    int t;
+    cin >> t;
 
-    for(int i=0;i<n;i++)
+    while(t--)
     {
+        int x;
         cin >> x;
-        v.push_back(x);
-    }
+        string s;
+        cin >> s;
+        int marks;
 
-    for(int i=0 ; i<v.size(); i++)
-        cout << v[i] << " " << endl; 
-
-    // //Searching
-    int s,f=0;
-    cin >> s;
-    for(int i=0;i<v.size();i++)
-    {
-        if(s==v[i])
-            f=1;
+        if(x==1)
+        {
+            cin >> marks;
+            m[s] = m[s] + marks;
+        }
+        else if(x==2)
+            m.erase(s);
+        else
+            cout << m[s] << endl;
     }
-    if(f==0)
-        cout << "Not Present" <<endl;
-    else
-        cout << "Present" <<endl;
-        
     return 0; 
 }
